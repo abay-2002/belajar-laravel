0be3de39b69e11b4547ff414e28f49a87c76e832
abay-2002 <akbar121202@gmail.com>
2023-02-24 11:10:00 +0800

    first commit

---
+++ belajar-laravel/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About page</title>
+</head>
+<body>
+    <h1>About page</h1>
+    <img src="img/akbar-angkasa.jpg" alt="akbar angkasa" width="200">
+    <p><?php echo $name; ?></p>
+    <p><?= $email; ?></p>
+</body>
+</html>

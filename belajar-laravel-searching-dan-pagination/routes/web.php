f55348fe976ea4f5a73534f8dcbac85d883f91af
abay-2002 <akbar121202@gmail.com>
2023-02-27 13:57:48 +0800

    belajar laravel searching dan pagination

---
+++ belajar-laravel-searching-dan-pagination/routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home",
+        'active' => 'home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        'active' => 'about',
+        "name" => "Akbar Angkasa",
+        "email" => "akbar121202@gmail.com",
+        "img" => "akbar-angkasa.jpg"
+    ]);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+
+// Route dibawah menggunakan Route Model Binding
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function(){
+    return view('categories', [
+        'title' => 'Post categories',
+        'active' => 'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+// Route::get('/categories/{category:slug}', function(Category $category){
+//     return view('posts', [
+//         'title' => "Post by Category: $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'user'),
+//     ]);
+// });
+
+// Route::get('/authors/{author:username}', function(User $author){
+//     return view('posts', [
+//         'title' => "Post by Author: $author->name",
+//         'active' => '',
+//         'posts' => $author->posts->load('category', 'user'),
+//     ]);
+// });

ae9001e91d06deef6c819e8d1c71c97d4202fa9c
abay-2002 <akbar121202@gmail.com>
2023-02-27 10:38:10 +0800

    redesign UI

--- belajar-laravel-factory-and-faker/resources/views/post.blade.php
+++ belajar-laravel-factory-and-faker/resources/views/post.blade.php
@@ -1,12 +1,23 @@
 @extends('layouts/main')
 
 @section('container')
-    <article>
-        <h2>{{ $post['title'] }}</h2>
-        <p>By. <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-        
-        {!! $post->body !!}
-    </article>
 
-    <a href="/posts" class="d-block mt-3">Kembali ke posts</a>
+    <div class="container">
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8">
+                <h2 class="mb-3">{{ $post['title'] }}</h2>
+                <p>By. <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+
+                <a href="/posts" class="d-block mt-3">Kembali ke posts</a>
+            </div>
+        </div>
+    </div>
+
+   
 @endsection
--- belajar-laravel-factory-and-faker/routes/web.php
+++ belajar-laravel-factory-and-faker/routes/web.php
@@ -18,13 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        'active' => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        'active' => 'about',
         "name" => "Akbar Angkasa",
         "email" => "akbar121202@gmail.com",
         "img" => "akbar-angkasa.jpg"
@@ -39,6 +41,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -46,6 +49,7 @@ Route::get('/categories', function(){
 Route::get('/categories/{category:slug}', function(Category $category){
     return view('posts', [
         'title' => "Post by Category: $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'user'),
     ]);
 });
@@ -53,6 +57,7 @@ Route::get('/categories/{category:slug}', function(Category $category){
 Route::get('/authors/{author:username}', function(User $author){
     return view('posts', [
         'title' => "Post by Author: $author->name",
+        'active' => '',
         'posts' => $author->posts->load('category', 'user'),
     ]);
 });
